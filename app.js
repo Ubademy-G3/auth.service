76c1d580fb3901cd0f6c7ed61d416de9ba51e278
chiabauni <chiabauni@gmail.com>
2021-10-10 12:42:09 -0300

    Corre localmente

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
 const express = require('express');
-const { Client } = require('pg');
 
 const app = express();
 
-var client;
-if (process.env.NODE_ENV !== 'stage') {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      query_timeout: 1000,
-      statement_timeout: 1000,
-      ssl: false
-    }); 
-} else {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      query_timeout: 1000,
-      statement_timeout: 1000,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-}
-
-client.connect();
+var client = require('mongoose');
+client.connect('mongodb://mongo/test', {useNewUrlParser: true});
 
 app.get('/ping', (req, res) => res.send('Pong!'));
 
-app.get('/status', (req, res) => 
-    client.query('SELECT NOW()', (err) => res.send({ service: 'UP', db: err ? 'DOWN' : 'UP' }))
-);
+//app.get('/status', (req, res) => 
+//    client.query('SELECT NOW()', (err) => res.send({ service: 'UP', db: err ? 'DOWN' : 'UP' }))
+//);
 
 app.listen(process.env.PORT, () => {
     console.log(`App running on port ${process.env.PORT}`); 
