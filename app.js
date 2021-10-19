c271179c937e6ea84e4d13e745d99331e70a1cb8
Daniela <daniela.leloutre@gmail.com>
2021-10-19 17:59:00 -0300

    agrego swagger

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const express = require('express');
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const client = require("mongoose");
+const swaggerDocument = require("./swagger.json");
+const auth = require("./infrastructure/routes/auth");
 
 const app = express();
 
-var client = require('mongoose');
+client.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
-client.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+app.use("/auth", auth);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/ping', (req, res) => res.send('Pong!'));
+app.get("/ping", (req, res) => res.send("Pong!"));
 
-//app.get('/status', (req, res) => 
+// app.get('/status', (req, res) =>
 //    client.query('SELECT NOW()', (err) => res.send({ service: 'UP', db: err ? 'DOWN' : 'UP' }))
-//);
+// );
 
 app.listen(process.env.PORT, () => {
-    console.log(`App running on port ${process.env.PORT}`); 
+  // console.log(`App running on port ${process.env.PORT}`);
 });
