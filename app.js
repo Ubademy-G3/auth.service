11c34a5205bc998e79db6930faa4d8941a51d54c
Daniela <daniela.leloutre@gmail.com>
2021-10-26 11:08:43 -0300

    login con tests y fixes de linter

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const auth = require("./infrastructure/routes/Auth");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/auth", auth);
--- test/auth.test.js
+++ test/auth.test.js
@@ -4,11 +4,26 @@ const app = require("../app");
 const model = require("../infrastructure/db/UserSchema");
 
 let mockedUser;
+let mockedHashedUser;
+let mockedTokenUser;
 
 beforeEach(() => {
   mockedUser = {
     email: "john@doe.com",
-    password: "testing123",
+    password: "test3",
+    salt: "e79d7530a0891964159f56f04d0785b1",
+  };
+  mockedHashedUser = {
+    email: "john@doe.com",
+    password: "140685ff8c604e2b58a6fa0a376f03af33ce502d1267d8c04835e22d7978ceeb9713118f1cc1813567ffd96c55a7e66839b8ec3ce7ad4070fa2981d2505b7e06",
+    salt: "e79d7530a0891964159f56f04d0785b1",
+  };
+  mockedTokenUser = {
+    id: 1234,
+    email: "john@doe.com",
+    password: "140685ff8c604e2b58a6fa0a376f03af33ce502d1267d8c04835e22d7978ceeb9713118f1cc1813567ffd96c55a7e66839b8ec3ce7ad4070fa2981d2505b7e06",
+    salt: "e79d7530a0891964159f56f04d0785b1",
+    token: "TOKEN123",
   };
 });
 
@@ -40,7 +55,7 @@ describe("POST /auth/signup", () => {
       .expect(400)
       .then((response) => {
         const res = JSON.parse(response.text);
-        expect(res.message).toBe("Bad request");
+        expect(res.message).toBe("Missing required fields");
       });
   });
   test("Fails when email is missing", async () => {
@@ -49,7 +64,7 @@ describe("POST /auth/signup", () => {
       .expect(400)
       .then((response) => {
         const res = JSON.parse(response.text);
-        expect(res.message).toBe("Bad request");
+        expect(res.message).toBe("Missing required fields");
       });
   });
   test("Fails email already exists", async () => {
@@ -62,6 +77,69 @@ describe("POST /auth/signup", () => {
       .then((response) => {
         const res = JSON.parse(response.text);
         expect(res.message).toBe("User already exists with given email");
+      });
+  });
+});
+
+describe("POST /auth/login", () => {
+  beforeEach(() => {
+    mockingoose(model).reset();
+    mockingoose(model).toReturn(mockedUser, "save");
+    process.env.JWT_SECRET_KEY = "test";
+  });
+  afterEach(() => {
+    delete process.env.JWT_SECRET_KEY;
+  });
+  test("User logs in successfully with valid email and password", async () => {
+    mockingoose(model).toReturn(mockedHashedUser, "findOne");
+    mockingoose(model).toReturn(mockedTokenUser, "findOneAndUpdate");
+    await supertest(app).post("/auth/login")
+      .send(mockedUser)
+      .expect(200)
+      .then((response) => {
+        const res = JSON.parse(response.text);
+        expect(res.email).toBe("john@doe.com");
+        expect(res.token).toBeDefined();
+        expect(res.password).toBeUndefined();
+        expect(res.salt).toBeUndefined();
+        expect(res.id).toBeUndefined();
+      });
+  });
+  test("Fails when password is missing", async () => {
+    await supertest(app).post("/auth/login")
+      .send({ email: mockedUser.email })
+      .expect(400)
+      .then((response) => {
+        const res = JSON.parse(response.text);
+        expect(res.message).toBe("Missing required fields");
+      });
+  });
+  test("Fails when email is missing", async () => {
+    await supertest(app).post("/auth/login")
+      .send({ password: mockedUser.password })
+      .expect(400)
+      .then((response) => {
+        const res = JSON.parse(response.text);
+        expect(res.message).toBe("Missing required fields");
+      });
+  });
+  test("Fails when email not found", async () => {
+    await supertest(app).post("/auth/login")
+      .send(mockedUser)
+      .expect(404)
+      .then((response) => {
+        const res = JSON.parse(response.text);
+        expect(res.message).toBe("User not found");
+      });
+  });
+  test("Fails when invalid password", async () => {
+    mockingoose(model).toReturn(mockedHashedUser, "findOne");
+    await supertest(app).post("/auth/login")
+      .send({ email: mockedUser.email, password: "bananas" })
+      .expect(403)
+      .then((response) => {
+        const res = JSON.parse(response.text);
+        expect(res.message).toBe("Unauthorized");
       });
   });
 });
